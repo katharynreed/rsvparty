421f71426237289b25f924b349831664ffc4af5b
Drew Devlin and Keith Evans <keith@>
2017-03-06 13:40:15 -0800

    add passing tests for Attendee save, getall deleteall

--- src/Attendee.php
+++ src/Attendee.php
@@ -1,6 +1,59 @@
 <?php
     class Attendee {
-      
+
+        private $name;
+        private $event_id;
+        private $id;
+
+        function __construct($name, $event_id, $id = null)
+        {
+            $this->name = $name;
+            $this->event_id = $event_id;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getEventId()
+        {
+            return $this->event_id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $save = $GLOBALS['DB']->prepare("INSERT INTO attendees (name, event_id) VALUES (:name, :event_id);");
+            $save->execute([':name' => $this->getName(), ':event_id' => $this->getEventId()]);
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_attendees = $GLOBALS['DB']->query("SELECT * FROM attendees;");
+            if ($returned_attendees) {
+                $attendees = $returned_attendees->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Attendee', ['name', 'event_id', 'id']);
+            } else {
+                $attendees = [];
+            }
+            return $attendees;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM attendees;");
+        }
     }
 
 ?>
--- tests/AttendeeTest.php
+++ tests/AttendeeTest.php
@@ -1,4 +1,70 @@
 <?php
-    
 
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Attendee.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=rsvparty_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class AttendeeTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+            Attendee::deleteAll();
+        }
+
+        function test_save()
+        {
+            $name = 'Geoff';
+            $event_id = '2';
+            $test_attendee = new Attendee($name, $event_id);
+
+            $test_attendee->save();
+            $result = Attendee::getAll();
+
+            $this->assertEquals($test_attendee, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $name = 'Geoff';
+            $event_id = '2';
+            $test_attendee = new Attendee($name, $event_id);
+            $test_attendee->save();
+
+            $name = 'Dave';
+            $event_id = '4';
+            $test_attendee2 = new Attendee($name, $event_id);
+            $test_attendee2->save();
+
+            $result = Attendee::getAll();
+
+            $this->assertEquals([$test_attendee, $test_attendee2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            $name = 'Geoff';
+            $event_id = '2';
+            $test_attendee = new Attendee($name, $event_id);
+            $test_attendee->save();
+
+            $name = 'Dave';
+            $event_id = '4';
+            $test_attendee2 = new Attendee($name, $event_id);
+            $test_attendee2->save();
+
+            Attendee::deleteAll();
+            $result = Attendee::getAll();
+
+            $this->assertEquals([], $result);
+        }
+    }
 ?>
