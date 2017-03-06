243bf23c82844ece2afa0bc7a9d4a00b597e1020
Brynna McCarter and Katharyn Reed <brynna+katharyn@>
2017-03-06 14:10:35 -0800

    add passing test for updateDateTime

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -168,6 +168,24 @@
             $this->assertEquals($new_location, $result);
         }
 
+        function test_updateDateT()
+        {
+            $user_id = '1';
+            $name = 'Sock Puppet Convention';
+            $date_time = '2017-10-10 13:30:00';
+            $description = 'Soft core puppet enthusiasts.';
+            $location = 'Portland, OR';
+            $test_event = new Event ($user_id, $name, $date_time, $description, $location);
+            $test_event->save();
+
+            $new_date_time = '2017-09-10 13:30:00';
+            $test_event->updateDateTime($new_date_time);
+
+            $result = $test_event->getDateTime();
+
+            $this->assertEquals($new_date_time, $result);
+        }
+
     }
 
 ?>
