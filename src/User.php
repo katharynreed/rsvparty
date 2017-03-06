20fa8e075fe3747ba296149ea4953f3736c56a29
Drew Devlin and Keith Evans <keith@>
2017-03-06 10:54:27 -0800

    add User class with save deleteAll and getAll

--- src/User.php
+++ src/User.php
@@ -1,6 +1,61 @@
 <?php
     class User {
+        private $name;
+        private $password;
+        private $guest_key;
+        private $id;
 
+        function __construct($name, $password, $guest_key = null, $id = null)
+        {
+            $this->name = $name;
+            $this->password = $password;
+            $this->id = $id;
+            $this->guest_key = $guest_key;
+        }
+
+        function setName($name) {
+            $this->name = $name;
+        }
+
+        function getName() {
+            return $this->name;
+        }
+
+        function setPassword($password) {
+            $this->password = $password;
+        }
+
+        function getPassword() {
+            return $this->password;
+        }
+
+        function getGuestKey() {
+            return $this->guest_key;
+        }
+
+        function getId() {
+            return $this->id;
+        }
+
+        function save() {
+            $save = $GLOBALS['DB']->prepare("INSERT INTO users (name, password, guest_key) VALUES (:name, :password, :guest_key);");
+            $save->execute([':name' => $this->getName(), ':password' => $this->getPassword(), ':guest_key' => $this->getGuestKey()]);
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll() {
+            $returned_users = $GLOBALS['DB']->query("SELECT * FROM users;");
+            if ($returned_users) {
+                $users = $returned_users->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'User', ['name', 'password', 'guest_key', 'id']);
+            } else {
+                $users = [];
+            }
+            return $users;
+        }
+
+        static function deleteAll() {
+            $GLOBALS['DB']->exec('DELETE FROM users');
+        }
     }
 
 
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,15 +1,75 @@
 <?php
-    require_once 'src/User.php';
 
-    class SourceTest extends PHPUnit_Framework_TestCase
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/User.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=rsvparty_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class UserTest extends PHPUnit_Framework_TestCase
     {
-        function test_source_function() {
-            $input = ' ';
-            $test_source = new Source;
+        protected function tearDown()
+        {
+            User::deleteAll();
+        }
 
-            $result = $test_source->test_function();
+        function test_save()
+        {
+            $name = 'Bob';
+            $password = 'pass';
+            $guest_key = '2';
+            $new_user = new User($name, $password, $guest_key);
+            $new_user->save();
 
-            $this->assertEquals(1, $result);
+            $result = User::getAll();
+
+            $this->assertEquals($new_user, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $name = 'Bob';
+            $password = 'pass';
+            $guest_key = '2';
+            $new_user = new User($name, $password, $guest_key);
+            $new_user->save();
+
+            $name2 = 'Bob2';
+            $password2 = 'pass2';
+            $guest_key2 = '22';
+            $new_user2 = new User($name2, $password2, $guest_key2);
+            $new_user2->save();
+
+            $result = User::getAll();
+
+            $this->assertEquals([$new_user, $new_user2], $result);
+
+        }
+
+        function test_deleteAll()
+        {
+            $name = 'Bob';
+            $password = 'pass';
+            $guest_key = '2';
+            $new_user = new User($name, $password, $guest_key);
+            $new_user->save();
+
+            $name2 = 'Bob2';
+            $password2 = 'pass2';
+            $guest_key2 = '22';
+            $new_user2 = new User($name2, $password2, $guest_key2);
+            $new_user2->save();
+
+            User::deleteAll();
+            $result = User::getAll();
+
+            $this->assertEquals([], $result);
         }
     }
 
