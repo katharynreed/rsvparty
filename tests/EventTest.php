13210f5c438a11f7acc41d9499261c0f3f567e9c
Brynna McCarter and Katharyn Reed <brynna+katharyn@>
2017-03-06 10:49:00 -0800

    create passing condition for setters test in event model

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -35,7 +35,21 @@
 
         function test_setters()
         {
+            $user_id = '1';
+            $name = 'Sock Puppet Convention';
+            $date_time = '20171010 13:30:00';
+            $description = 'Soft core puppet enthusiasts.';
+            $location = 'Portland, OR';
+            $id = '1';
+            $test_event = new Event ($user_id, $name, $date_time, $description, $location, $id);
 
+            $test_event->setName('Sock Puppet Annual Event');
+            $test_event->setDescription('Soft core puppet enthusiasts -- no weird stuff.');
+            $test_event->setLocation('Gresham, OR');
+
+            $result = array($test_event->getName(), $test_event->getDescription(), $test_event->getLocation());
+            $expected_result = array('Sock Puppet Annual Event', 'Soft core puppet enthusiasts -- no weird stuff.', 'Gresham, OR');
+            $this->assertEquals($result, $expected_result);
         }
 
     }
